e97c34b39c9b86bb676fbabb6c0c7e9e629e78db
gabang2 <offbeat1020@naver.com>
2023-07-28 16:24:00 +0900

    Feat : 정확도 향상을 위해서 Teachable Machine 모델로 교체

--- FoodDetectionAPI/classifier/classifier.py
+++ FoodDetectionAPI/classifier/classifier.py
@@ -3,29 +3,33 @@ from PIL import Image
 import numpy as np
 
 def predict(file_name):
-    # yolo 관련 파일 불러오기
-    model = torch.hub.load('ultralytics/yolov5', 'custom', path='./classifier/best.pt', force_reload=True)
+    file_name = '.' + str(file_name)
+    print(file_name)
+    np.set_printoptions(suppress=True)
+    model = load_model("./classifier/keras_model.h5", compile=False)
+    class_names = open("./classifier/labels.txt", "r").readlines()
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
-    # 이미지 불러오기
-    file_name = "." + file_name
-    img = Image.open(file_name).convert("RGB")
-    img = np.array(img)
+    # Replace this with the path to your image
+    image = Image.open(file_name).convert("RGB")
 
-    # 결과 받아옴
-    detect = model(img).pandas().xyxy[0].to_numpy()
-    result = list(set([i[6] for i in detect]))
+    # resizing the image to be at least 224x224 and then cropping from the center
+    size = (224, 224)
+    image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
 
-    ingredient_dict = {'bean sprouts': '콩나물',
-                       'beef': '소고기',
-                       'chicken': '닭고기',
-                       'egg': '계란',
-                       'fork': '포크',
-                       'garlic': '마늘',
-                       'green onion': '파',
-                       'kimchi': '김치',
-                       'onion': '양파',
-                       'potato': '감자',
-                       'spam': '스팸'
-                       }
-    result = [ingredient_dict[i] for i in result]
+    # turn the image into a numpy array
+    image_array = np.asarray(image)
+
+    # Normalize the image
+    normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
+
+    # Load the image into the array
+    data[0] = normalized_image_array
+
+    # Predicts the model
+    prediction = model.predict(data)
+    index = np.argmax(prediction)
+    class_name = class_names[index]
+    result = list()
+    result.append(class_name[2:].replace('\n', ''))
     return result
